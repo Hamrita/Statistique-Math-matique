2c4eda56287d11cfb6710771014fadf732aadf0d
Hamrita <mhamrita@gmail.com>
2021-09-15 16:33:36 +0100

    update Rcode_chap1

--- Rcode_Chap1.R
+++ Rcode_Chap1.R
@@ -1,10 +1,20 @@
 # Fonction de répartition (v.a.d)
 x=c(0,1,2,3)
 p=c(0.1,0.25,0.3,0.35)
-plot(x,p,type="s", lwd=3, col=4)
+Fx=cumsum(p)
+Fx
+plot(x,p,type="s", lwd=3, col=2, xlab="x", ylab=expression(F(x)))
 
 # Fonction de répartition de la loi uniforme [0,1]
 x=seq(-1,2,len=10) # séquence de -1 jusqu'à 2 de longueur 10
 Fx=dunif(x, min=0, max=1)
 Fx   # Afficher les valeurs de F(x)
-plot(x,Fx,type="s", lwd=3, col=4)
+plot(x,Fx,type="s", lwd=3, col=2, xlab="x", ylab=expression(F(x)))
+
+# Fonction de répartition de la loi exponentielle (lambda=2)
+
+x=seq(0,4, len=100)
+Fx=dexp(x,2)
+plot(x,Fx,type="l", lwd=3, col=2, xlab="x", ylab=expression(F(x)))
+# ou encore
+curve(dexp(x,2),0,4,lwd=3, col=2, xlab="x", ylab=expression(F(x)))
