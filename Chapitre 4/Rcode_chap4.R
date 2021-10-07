dbbf7b3b7c1e3dc9b8734dda7fc94a6b96fab099
Hamrita <mhamrita@gmail.com>
2021-10-07 19:12:19 +0100

    update Rcode

--- Chapitre 4/Rcode_chap4.R
+++ Chapitre 4/Rcode_chap4.R
@@ -23,3 +23,18 @@ Zn=function(n) cummax(runif(n))
 check.convergence(10000,1000,Yn,mode="p")  # Yn cvge en prob vers 0
 
 check.convergence(10000,1000,Zn,mode="p")  # Zn cvge en prob vers 1
+
+######################################
+#  Convergence en moyenne et 
+#   en moyenne quadratique
+######################################
+
+# Exercice 2: Xn ~ U[0,1/n]
+
+Xn=function(n) runif(1/(1:n))
+
+check.convergence(10000,1000,Xn,mode="r", r=1)  # en moyenne
+
+check.convergence(10000,1000,Xn,mode="r", r=2)  # en m.q
+
+check.convergence(10000,1000,Xn,mode="r", r=3)  # en m. d'ordre 3
